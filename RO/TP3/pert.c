e79098a6dacd45e652619e395e568b2ef80096c4
ZeCroque <zobilezeub@gmail.com>
2019-05-09 01:45:29 +0200

    RO_09/05/2019_1:45

--- RO/TP3/pert.c
+++ RO/TP3/pert.c
@@ -14,11 +14,9 @@ typedef struct
   int num_tache;
 } tache;
 
-tache *projet;
-int **matrice;
-
 int main()
 {
+  //Déclaration des variables
   int chemin[100];
   int associe [100];
   int sommeChemin = 0;
@@ -26,7 +24,10 @@ int main()
   int i,j,k;
   int somme = 0;
   int *pile;
+  tache *projet;
+  int **matrice;
 
+  //Instanciation dynamique & initialisation
   projet = (tache *)malloc((nbTaches + 2) * sizeof(tache));
   matrice = (int **)malloc((nbTaches + 2) * sizeof(int *));
 
@@ -42,78 +43,78 @@ int main()
       matrice[i][j] = -1;
     }
   }
-
-  matrice[0][1] = 0;
-
+  matrice[0][1]=0;
   pile = (int *)malloc((nbTaches +2) * sizeof(int));
 
-  matrice[0][2]=0;
+  //Remplissage de la matrice
+  matrice[1][2]=0;
+  matrice[1][3]=0;
 
-  matrice[1][3]=3;
-  matrice[1][4]=3;
-  matrice[1][6]=3;
-  matrice[1][7]=3;
-  matrice[1][8]=3;
-  matrice[1][10]=3;
-  matrice[1][11]=3;
-  matrice[1][12]=3;
-  matrice[1][13]=3;
-  matrice[1][14]=3;
+  matrice[2][4]=3;
+  matrice[2][5]=3;
+  matrice[2][7]=3;
+  matrice[2][8]=3;
+  matrice[2][9]=3;
+  matrice[2][10]=3;
+  matrice[2][11]=3;
+  matrice[2][12]=3;
+  matrice[2][13]=3;
+  matrice[2][14]=3;
+  matrice[2][15]=3;
 
-  matrice[2][3]=4;
-  matrice[2][4]=4;
-  matrice[2][5]=4;
-  matrice[2][10]=4;
-  matrice[2][11]=4;
-  matrice[2][12]=4;
-  matrice[2][13]=4;
-  matrice[2][14]=4;
+  matrice[3][4]=4;
+  matrice[3][5]=4;
+  matrice[3][6]=4;
+  matrice[3][11]=4;
+  matrice[3][12]=4;
+  matrice[3][13]=4;
+  matrice[3][14]=4;
+  matrice[3][15]=4;
 
-  matrice[3][14]=7;
+  matrice[4][15]=7;
 
-  matrice[4][11]=1;
-  matrice[4][12]=1;
-  matrice[4][13]=1;
-  matrice[4][13]=1;
+  matrice[5][12]=1;
+  matrice[5][13]=1;
+  matrice[5][14]=1;
+  matrice[5][15]=1;
 
-  matrice[5][7]=8;
-  matrice[5][10]=8;
-  matrice[5][14]=8;
+  matrice[6][8]=8;
+  matrice[6][11]=8;
+  matrice[6][15]=8;
 
-  matrice[6][8]=5;
-  matrice[6][9]=5;
-  matrice[6][10]=5;
-  matrice[6][14]=5;
+  matrice[7][9]=5;
+  matrice[7][10]=5;
+  matrice[7][11]=5;
+  matrice[7][15]=5;
 
-  matrice[7][10]=8;
-  matrice[7][14]=8;
-
-  matrice[8][9]=8;
-  matrice[8][10]=8;
   matrice[8][11]=8;
-  matrice[8][14]=8;
+  matrice[8][15]=8;
 
-  matrice[9][10]=5;
-  matrice[9][14]=5;
+  matrice[9][10]=8;
+  matrice[9][11]=8;
+  matrice[9][12]=8;
+  matrice[9][15]=8;
 
-  matrice[10][14]=7;
+  matrice[10][11]=5;
+  matrice[10][15]=5;
 
-  matrice[11][12]=6;
-  matrice[11][14]=6;
+  matrice[11][15]=7;
 
-  matrice[12][14]=2;
+  matrice[12][13]=6;
+  matrice[12][15]=6;
 
-  matrice[13][14]=7;
+  matrice[13][15]=2;
 
-  matrice[14][15]=4;
+  matrice[14][15]=7;
 
-  matrice[nbTaches][nbTaches+1] = 1;
+  matrice[15][16]=4;
+
+  matrice[nbTaches][nbTaches+1] = 0;
 
   //Dates au plus tôt
-  printf("\n===DATES AU PLUS TOT===\n");
   projet[0].datePlusTot = 0;
 
-  for(j = 1; j < nbTaches + 2; j++)
+  for(j = 0; j < nbTaches+2; j++)
   {
     projet[j].datePlusTot = 0;
     for(i = 0; i < j; i++)
@@ -123,13 +124,11 @@ int main()
         projet[j].datePlusTot = max(projet[j].datePlusTot, projet[i].datePlusTot + matrice[i][j]);
       }
     }
-    printf(" \nLa date au plus tôt de %d est %d", j, projet[j].datePlusTot);
   }
 
   projet[nbTaches+1].datePlusTard = projet[nbTaches+1].datePlusTot;
 
   //Dates au plus tard
-  printf("\n\n===DATES AU PLUS TARD===\n");
   for(i = nbTaches ; i >= 0; i--)
   {
     projet[i].datePlusTard = INT_MAX;
@@ -137,20 +136,18 @@ int main()
     {
       if(matrice[i][j] != -1)
       {
-         projet[i].datePlusTard  = min (projet[j].datePlusTard  -  matrice[i][j], projet[i].datePlusTard);
+         projet[i].datePlusTard  = min(projet[j].datePlusTard  -  matrice[i][j], projet[i].datePlusTard);
       }
     }
-    printf("\nLa date au plus tard de %d est %d", i, projet[i].datePlusTot);
   }
 
   //Marges
-  printf("\n\n===MARGES===\n");
   for(i=0; i < nbTaches +2; i++)
   {
     projet[i].marge = projet[i].datePlusTard - projet[i].datePlusTot;
-    printf("\nLa marge de %d est de %d", i, projet[i].marge);
   }
 
+  //Chemin critique
   associe[sommeChemin] = -1;
   pile[somme] = 1;
   somme ++;
@@ -174,11 +171,6 @@ int main()
         }
         else
         {
-          printf("\n ==> ");
-          for (k = 1; k < sommeChemin; k++)
-          {
-            printf(" %d ", chemin[k]);
-          }
           for (k = 0; k < sommeChemin; k++)
           {
             if(somme > 0 && chemin[k] ==  associe[somme - 1])
@@ -192,6 +184,49 @@ int main()
     }
   }
 
-  printf("\n");
+  //Affichage
+  printf("\n===Tableau des marges===\n\n\t+Tot\t+Tard\tMarge\n");
+  for(i=1; i<nbTaches+1; i++)
+  {
+    if(i==1)
+    {
+      printf("Deb\t%d\t%d\t%d\n",projet[i].datePlusTot,projet[i].datePlusTard,projet[i].marge);
+    }
+    else if(i<nbTaches)
+    {
+      printf("%c\t%d\t%d\t%d\n",'A'+i-2,projet[i].datePlusTot,projet[i].datePlusTard,projet[i].marge);
+    }
+    else
+    {
+      printf("Fin\t%d\t%d\t%d\n",projet[i].datePlusTot,projet[i].datePlusTard,projet[i].marge);
+    }
+
+  }
+
+  for (k = 1; k < sommeChemin; k++)
+  {
+    if(chemin[k]==1)
+    {
+      printf("\n\n===Chemin critique===\n\nDeb ");
+    }
+    else if(chemin[k]<nbTaches)
+    {
+      printf( " %c ",'A'+chemin[k]-2);
+    }
+    else
+    {
+      printf(" Fin\n");
+    }
+  }
+
+  //Libération de la mémoire
+  free(pile);
+  free(projet);
+  for(i=0; i<nbTaches+2;i++)
+  {
+    free(matrice[i]);
+  }
+  free(matrice);
+
   return 0;
 }
